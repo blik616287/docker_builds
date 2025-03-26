119bc3955f7ea4523ee6f37f5d74832e92074148
Marty Forde <marty@insightsoftmax.com>
2025-03-25 20:06:22 -0500

    forde-commit-1742951182

--- amazonlinux/2023/openfoam/12/motorBike/rip_and_tear.sh
+++ amazonlinux/2023/openfoam/12/motorBike/rip_and_tear.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-./config.py --disable-ssl --mpi-image blik6126287/amazonlinux2023_openfoam12:motorBike_01_Allclean
-./config.py --disable-ssl --mpi-image blik6126287/amazonlinux2023_openfoam12:motorBike_02_data_setup
-./config.py --disable-ssl --mpi-image blik6126287/amazonlinux2023_openfoam12:motorBike_03_blockMesh
-./config.py --disable-ssl --mpi-image blik6126287/amazonlinux2023_openfoam12:motorBike_04_decomposePar
-./config.py --disable-ssl --mpi-processes 8 --mpi-image blik6126287/amazonlinux2023_openfoam12:motorBike_05_parallel_snappyHexMesh
-./config.py --disable-ssl --mpi-processes 1 --mpi-image blik6126287/amazonlinux2023_openfoam12:motorBike_06_rmexec
-./config.py --disable-ssl --mpi-processes 8 --mpi-image blik6126287/amazonlinux2023_openfoam12:motorBike_07_parallel_renumberMesh
-./config.py --disable-ssl --mpi-processes 8 --mpi-image blik6126287/amazonlinux2023_openfoam12:motorBike_08_parallel_potentialFoam
-./config.py --disable-ssl --mpi-processes 8 --mpi-image blik6126287/amazonlinux2023_openfoam12:motorBike_09_parallel_foamRun
+./submit.py --disable-ssl --mpi-image blik6126287/amazonlinux2023_openfoam12:motorBike_01_Allclean
+./submit.py --disable-ssl --mpi-image blik6126287/amazonlinux2023_openfoam12:motorBike_02_data_setup
+./submit.py --disable-ssl --mpi-image blik6126287/amazonlinux2023_openfoam12:motorBike_03_blockMesh
+./submit.py --disable-ssl --mpi-image blik6126287/amazonlinux2023_openfoam12:motorBike_04_decomposePar
+./submit.py --disable-ssl --mpi-processes 8 --mpi-image blik6126287/amazonlinux2023_openfoam12:motorBike_05_parallel_snappyHexMesh
+./submit.py --disable-ssl --mpi-processes 1 --mpi-image blik6126287/amazonlinux2023_openfoam12:motorBike_06_rmexec
+./submit.py --disable-ssl --mpi-processes 8 --mpi-image blik6126287/amazonlinux2023_openfoam12:motorBike_07_parallel_renumberMesh
+./submit.py --disable-ssl --mpi-processes 8 --mpi-image blik6126287/amazonlinux2023_openfoam12:motorBike_08_parallel_potentialFoam
+./submit.py --disable-ssl --mpi-processes 8 --mpi-image blik6126287/amazonlinux2023_openfoam12:motorBike_09_parallel_foamRun
